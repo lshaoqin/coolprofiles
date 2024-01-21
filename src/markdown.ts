ee4a0a1854fc18a96c9ba466d89c89fc878d3981
Kok Liang <limkokliang01@gmail.com>
2024-01-21 11:03:16 +0800

    added wordcloud output file to markdown

--- src/markdown.ts
+++ src/markdown.ts
@@ -20,6 +20,9 @@ export function generateMarkdown(contributions: string) {
     return `
 ## Contributions in the past 3 months
 ${contributions}
+
+![Image Alt Text](../wordcloud/out.jpg)
+
 This README was automatically generated using [coolprofiles](https://github.com/lshaoqin/coolprofiles)!
 `;
 }
