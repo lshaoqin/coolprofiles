82bccb0d11718517bab4d652941e013c6141cae9
lshaoqin <shaoqin.lu@gmail.com>
2024-01-21 11:15:40 +0800

    minor text changes

--- src/markdown.ts
+++ src/markdown.ts
@@ -1,7 +1,9 @@
 export function generateDropdown(title: string, readmeSummary: string, commitsSummary: string, url: string) {
     return `
 <details>
-<summary>${title}</summary>
+<summary>
+# ${title}
+</summary>
 Link to repo: ${url}
 <br/>
 ${readmeSummary}
@@ -23,9 +25,9 @@ export function generateDropdowns(entries: { [name: string]: string }) {
 
 export function generateMarkdown(contributions: string) {
     return `
-## Contributions in the past 3 months
+## 🔨 I've been working on:
 ${contributions}
 
-This README was automatically generated using [coolprofiles](https://github.com/lshaoqin/coolprofiles)!
+This README was automatically generated using [coolprofiles](https://github.com/lshaoqin/coolprofiles). Because of this, some of the information here may not be accurate - check out the repositories for yourself to find out more!
 `;
 }
