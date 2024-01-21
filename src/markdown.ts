0849cbbbdad5d713bd73e6e8f8a4c64bdb7e396e
lshaoqin <shaoqin.lu@gmail.com>
2024-01-21 10:45:34 +0800

    add additional sections to readme

--- src/markdown.ts
+++ src/markdown.ts
@@ -1,17 +1,19 @@
-function generateDropdown(title: string, content: string) {
+export function generateDropdown(title: string, readmeSummary: string, commitsSummary: string, url: string) {
     return `
 <details>
 <summary>${title}</summary>
-
-${content}
+Link to repo: ${url}
+${readmeSummary}
+---
+${commitsSummary}
 </details>
 `;
 }
 
-export function generateDropdowns(entries: { [key: string]: string }) {
+export function generateDropdowns(entries: { [name: string]: string }) {
     let dropdowns = "";
     for (const [key, value] of Object.entries(entries)) {
-        dropdowns += generateDropdown(key, value);
+        dropdowns += value;
     }
     return dropdowns;
 }
@@ -20,6 +22,7 @@ export function generateMarkdown(contributions: string) {
     return `
 ## Contributions in the past 3 months
 ${contributions}
+
 This README was automatically generated using [coolprofiles](https://github.com/lshaoqin/coolprofiles)!
 `;
 }
